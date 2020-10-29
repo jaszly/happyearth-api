4d6d331a960fdabab134043106d3a6191871546e
jaszly <jasvmurray@gmail.com>
2020-10-29 16:08:56 +1100

    add features and products to spots mongoose model

--- models/spots.js
+++ models/spots.js
@@ -1,64 +1,86 @@
-const mongoose = require('../database')
+const mongoose = require("../database");
 
-const Spots = mongoose.model('spots', {
-	images: {
-		type: [String],
-		required: true
-	},
-	title: {
-		type: String,
-		required: true
-	},
-	spotters: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'users'
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	types: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'types'
-	},
-	toggleEatins: {
-		type: Boolean,
-		default: false
-	},
-	eatins: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'eatins'
-		}
-	],
-	toggleTakeaways: {
-		type: Boolean,
-		default: false
-	},
-	takeaways: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'takeaways'
-		}
-	],
-	city: {
-		type: String,
-		required: [true, 'City is required.']
-	},
-	country: {
-		type: String,
-		required: [true, 'Country is required.']
-	},
-	center: {
-		lng: {
-			type: Number,
-			required: true
-		},
-		lat: {
-			type: Number,
-			required: true
-		}
-	}
-})
+const Spots = mongoose.model("spots", {
+  images: {
+    type: [String],
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  spotters: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  types: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "types",
+  },
 
-module.exports = Spots
+  toggleFeatures: {
+    type: Boolean,
+    default: false,
+  },
+  features: [
+    {
+      type: mongoose.Schema.Types.ObjectsId,
+      ref: "features",
+    },
+  ],
+  toggleProducts: {
+    type: Boolean,
+    default: false,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "products",
+    },
+  ],
+
+  toggleEatins: {
+    type: Boolean,
+    default: false,
+  },
+  eatins: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "eatins",
+    },
+  ],
+  toggleTakeaways: {
+    type: Boolean,
+    default: false,
+  },
+  takeaways: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "takeaways",
+    },
+  ],
+  city: {
+    type: String,
+    required: [true, "City is required."],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required."],
+  },
+  center: {
+    lng: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+
+module.exports = Spots;
