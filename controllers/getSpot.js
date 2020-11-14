b28d39cc2c0d35be17ad1f13f767f3ae1503f34c
jaszly <jasvmurray@gmail.com>
2020-11-14 20:59:05 +1100

    update to add features

--- controllers/getSpot.js
+++ controllers/getSpot.js
@@ -3,7 +3,7 @@ const Spots = require("../models/spots");
 module.exports = (req, res) => {
   console.log("connected");
   Spots.findById(req.params.id)
-    .populate("users eatins takeaways types")
+    .populate("users eatins takeaways types features")
     .lean()
     .then((data) => {
       console.log("req.params.id", req.params.id);
