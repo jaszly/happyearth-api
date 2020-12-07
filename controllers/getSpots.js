22dae00faae7d1c9a6f62ba8fc67832cc5c97868
jaszly <jasvmurray@gmail.com>
2020-12-07 21:36:08 +1100

    update getSpots

--- controllers/getSpots.js
+++ controllers/getSpots.js
@@ -1,16 +1,16 @@
-require('../database')
-const Spots = require('../models/spots')
+require("../database");
+const Spots = require("../models/spots");
 
 module.exports = (req, res) => {
-	Spots.find(req.query)
-		.populate('types')
-		.then(spots => {
-			spots.map(spot => {
-				spot.image = spot.images[0]
-				delete spot.images
-				return spot
-			})
-			res.send(spots)
-		})
-		.catch(err => res.send(err))
-}
+  Spots.find(req.query)
+    // .populate('types')
+    .then((spots) => {
+      spots.map((spot) => {
+        spot.image = spot.images[0];
+        delete spot.images;
+        return spot;
+      });
+      res.send(spots);
+    })
+    .catch((err) => res.send(err));
+};
