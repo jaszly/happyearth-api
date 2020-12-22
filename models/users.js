01f957680ea95c0431af7dff2e7ff9a12d28f165
jaszly <jasvmurray@gmail.com>
2020-12-22 15:21:42 +1000

    updated user model to include other user details

--- models/users.js
+++ models/users.js
@@ -1,27 +1,33 @@
-const mongoose = require('../database')
+const mongoose = require("../database");
 
-const Users = mongoose.model('users', {
-	firstName: {
-		type: String,
-		required: [true, 'First name is required']
-	},
-	lastName: {
-		type: String,
-		required: [true, 'Last name is required']
-	},
-	residenceCountry: {
-		type: String,
-		required: [true, 'Country of Residence is required']
-	},
-	email: {
-		type: String,
-		required: [true, 'Email is required']
-	},
-	password: {
-		type: String,
-		required: [true, 'Password is required']
-	},
-	avatar: String
-})
+const Users = mongoose.model(
+  "users",
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    residenceCountry: {
+      type: String,
+      required: [true, "Country of Residence is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    avatar: String,
+  },
+  (bio: {
+    type: String,
+  })
+);
 
-module.exports = Users
+module.exports = Users;
