649c9e0661bafd71a12ad8a91b615f723c9db4cc
jaszly <jasvmurray@gmail.com>
2020-12-29 08:10:07 +1000

    schema updates

--- models/users.js
+++ models/users.js
@@ -1,33 +1,27 @@
 const mongoose = require("../database");
 
-const Users = mongoose.model(
-  "users",
-  {
-    firstName: {
-      type: String,
-      required: [true, "First name is required"],
-    },
-    lastName: {
-      type: String,
-      required: [true, "Last name is required"],
-    },
-    residenceCountry: {
-      type: String,
-      required: [true, "Country of Residence is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    avatar: String,
-  },
-  (bio: {
+const Users = mongoose.model("users", {
+  firstName: {
     type: String,
-  })
-);
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  residenceCountry: {
+    type: String,
+    required: [true, "Country of Residence is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  avatar: String,
+});
 
 module.exports = Users;
