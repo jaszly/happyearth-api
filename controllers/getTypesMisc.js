6e94fa78bd1e5313da627c1e4ee5c547ca37bfc9
jaszly <jasvmurray@gmail.com>
2021-02-03 10:45:47 +1000

    change to category name
    types sorting by category name

--- controllers/getTypesMisc.js
+++ controllers/getTypesMisc.js
@@ -1,7 +1,7 @@
 const Types = require("../models/types");
 
 module.exports = (req, res) => {
-  Types.find({ category: "Misc" })
+  Types.find({ categoryname: "Misc" })
     .sort({ name: 1 })
     .then((data) => {
       res.send(data);
