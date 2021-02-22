494896b90101c38caa2a740e379dc2dec747ca1d
jaszly <jasvmurray@gmail.com>
2021-02-22 14:00:47 +1000

    sorted in alphabetical order

--- controllers/getTakeaways.js
+++ controllers/getTakeaways.js
@@ -1,15 +1,16 @@
-const Takeaways = require('../models/takeaways')
+const Takeaways = require("../models/takeaways");
 
 module.exports = (req, res) => {
-	console.log(Takeaways.find({}))
-	Takeaways.find({})
-		.then(data => {
-			console.log('takeaway works')
-			console.log(data)
-			res.send(data)
-		})
-		.catch(err => {
-			console.log('takeway doesnt work')
-			res.send(err)
-		})
-}
+  console.log(Takeaways.find({}));
+  Takeaways.find({})
+    .sort({ explanation: 1 })
+    .then((data) => {
+      console.log("takeaway works");
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log("takeway doesnt work");
+      res.send(err);
+    });
+};
