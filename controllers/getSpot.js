9c0b66d52542b66c70ada41a018b2763cfb2225e
jaszly <jasvmurray@gmail.com>
2021-02-22 14:00:07 +1000

    added ethics and diets to spot

--- controllers/getSpot.js
+++ controllers/getSpot.js
@@ -3,7 +3,7 @@ const Spots = require("../models/spots");
 module.exports = (req, res) => {
   // console.log("connected");
   Spots.findById(req.params.id)
-    .populate("users eatins takeaways types features")
+    .populate("users eatins takeaways types ethics diets")
     .lean()
     .then((data) => {
       console.log("req.params.id", req.params.id);
